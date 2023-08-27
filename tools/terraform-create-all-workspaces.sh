892c5e4ff90bfec83a1aa804c2d12c0610342099
cloudmiketyson <134102900+cloudmiketyson@users.noreply.github.com>
2023-08-27 16:11:32 -0400

    Update terraform-create-all-workspaces.sh

--- tools/terraform-create-all-workspaces.sh
+++ tools/terraform-create-all-workspaces.sh
@@ -6,5 +6,5 @@ file_locations=$(find . -maxdepth 2 -type f \( -name "backend.tf" \))
 for f in $file_locations
 do
     TF_MODULE_PATH=$(dirname $f) bash -c \
-        "$(curl -fsSL https://raw.githubusercontent.com/dyordsabuzo/miscellany/main/tools/terraform-workspace-local.sh)"
+        "$(curl -fsSL https://raw.githubusercontent.com/cloudmiketyson/miscellanious/main/tools/terraform-create-all-workspaces.sh)"
 done
